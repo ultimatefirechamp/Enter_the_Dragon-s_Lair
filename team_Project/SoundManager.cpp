2dfece7c4d0f7bcaf1ff5781a95717fb3835a40f
JYBbear <wnsduf_123@naver.com>
2023-05-30 17:27:24 +0900

    효과음추가

--- team_Project/SoundManager.cpp
+++ team_Project/SoundManager.cpp
@@ -29,20 +29,34 @@ SoundManager::SoundManager() {
 		std::cout << "Mix_LoadMUS(\"gameover.mp3\"): " << Mix_GetError() << std::endl;
 	}
 	Mix_VolumeMusic(VOLUME);
-	btn_click_se = Mix_LoadWAV("./resource/button_click.mp3");
-	btn_hold_se = Mix_LoadWAV("./resource/button_hold.mp3");
-	move_se = Mix_LoadWAV("./resource/move.mp3");
+	btn_click_se = Mix_LoadWAV("./resource/button_click.wav");
+	btn_hold_se = Mix_LoadWAV("./resource/button_hold.wav");
+	move_se = Mix_LoadWAV("./resource/move.wav");
 	player_atk_se = Mix_LoadWAV("./resource/player_attack.wav");
 	player_dmg_se = Mix_LoadWAV("./resource/player_damaged.wav");
+	player_die_se = Mix_LoadWAV("./resource/player_die.wav");
 	monster_atk_se = Mix_LoadWAV("./resource/monster_attack.wav");
 	monster_dmg_se = Mix_LoadWAV("./resource/monster_damaged.wav");
+	monster_die_se = Mix_LoadWAV("./resource/monster_die.wav");
+	phase_transition_se = Mix_LoadWAV("./resource/phase_transition.wav");
+	charge_se = Mix_LoadWAV("./resource/charge.wav");
+	skill1_se = Mix_LoadWAV("./resource/skill1.wav");
+	skill2_se = Mix_LoadWAV("./resource/skill2.wav");
+	skill3_se = Mix_LoadWAV("./resource/skill3.wav");
 	Mix_VolumeChunk(btn_click_se, SE_VOLUME);
 	Mix_VolumeChunk(btn_hold_se, SE_VOLUME);
 	Mix_VolumeChunk(move_se, SE_VOLUME);
 	Mix_VolumeChunk(player_atk_se, SE_VOLUME);
 	Mix_VolumeChunk(player_dmg_se, SE_VOLUME);
+	Mix_VolumeChunk(player_die_se, SE_VOLUME);
 	Mix_VolumeChunk(monster_atk_se, SE_VOLUME);
 	Mix_VolumeChunk(monster_dmg_se, SE_VOLUME);
+	Mix_VolumeChunk(monster_die_se, SE_VOLUME);
+	Mix_VolumeChunk(phase_transition_se, SE_VOLUME);
+	Mix_VolumeChunk(charge_se, SE_VOLUME);
+	Mix_VolumeChunk(skill1_se, SE_VOLUME);
+	Mix_VolumeChunk(skill2_se, SE_VOLUME);
+	Mix_VolumeChunk(skill3_se, SE_VOLUME);
 }
 
 SoundManager::~SoundManager() {
@@ -66,6 +80,7 @@ void SoundManager::intro_music()
 	}
 	Mix_PlayMusic(intro_bgm, -1);
 }
+
 //게임 내 bgm 재생
 void SoundManager::stage_music()
 {
@@ -74,6 +89,7 @@ void SoundManager::stage_music()
 	}
 	Mix_PlayMusic(stage_bgm, -1);
 }
+
 //엔딩 화면 bgm 재생
 void SoundManager::ending_music()
 {
@@ -82,6 +98,7 @@ void SoundManager::ending_music()
 	}
 	Mix_PlayMusic(ending_bgm, -1);
 }
+
 //게임오버 화면 bgm 재생
 void SoundManager::gameover_music()
 {
@@ -90,38 +107,87 @@ void SoundManager::gameover_music()
 	}
 	Mix_PlayMusic(gameover_bgm, -1);
 }
+
+//페이즈 전환시 효과음 재생
+void SoundManager::phase_transition_sound()
+{
+	Mix_PlayChannel(1, phase_transition_se, 0);
+}
+
 //버튼 클릭 효과음 재생
 void SoundManager::btn_click_sound()
 {
 	Mix_PlayChannel(-1, btn_click_se, 0);
 }
+
 //버튼 홀드 효과음 재생
 void SoundManager::btn_hold_sound()
 {
 	Mix_PlayChannel(-1, btn_hold_se, 0);
 }
+
 //움직일 때 발소리 재생
 void SoundManager::move_sound()
 {
 	Mix_PlayChannel(-1, move_se, 0);
 }
+
 //플레이어가 공격 시 효과음 재생
 void SoundManager::player_atk_sound()
 {
 	Mix_PlayChannel(-1, player_atk_se, 0);
 }
+
 //플레이어가 피격 시 효과음 재생
 void SoundManager::player_dmg_sound()
 {
 	Mix_PlayChannel(-1, player_dmg_se, 0);
 }
+
+//플레이어 사망 시 효과음 재생
+void SoundManager::player_die_sound()
+{
+	Mix_PlayChannel(1, player_die_se, 0);
+}
+
 //몬스터가 공격 시 효과음 재생
 void SoundManager::monster_atk_sound()
 {
 	Mix_PlayChannel(-1, monster_atk_se, 0);
 }
+
 //몬스터가 피격 시 효과음 재생
 void SoundManager::monster_dmg_sound()
 {
 	Mix_PlayChannel(-1, monster_dmg_se, 0);
+}
+
+//몬스터가 사망 시 효과음 재생
+void SoundManager::monster_die_sound()
+{
+	Mix_PlayChannel(-1, monster_die_se, 0);
+}
+
+//스킬 준비 자세 효과음 재생
+void SoundManager::charge_sound()
+{
+	Mix_PlayChannel(-1, charge_se, 0);
+}
+
+//스킬 1 효과음 재생
+void SoundManager::skill1_sound()
+{
+	Mix_PlayChannel(-1, skill1_se, 0);
+}
+
+//스킬 2 효과음 재생
+void SoundManager::skill2_sound()
+{
+	Mix_PlayChannel(-1, skill2_se, 0);
+}
+
+//스킬 3 효과음 재생
+void SoundManager::skill3_sound()
+{
+	Mix_PlayChannel(-1, skill3_se, 0);
 }
--- team_Project/SoundManager.h
+++ team_Project/SoundManager.h
@@ -11,24 +11,37 @@ public:
 	void stage_music();
 	void ending_music();
 	void gameover_music();
+	void phase_transition_sound();
 	void move_sound();
 	void btn_click_sound();
 	void btn_hold_sound();
 	void player_atk_sound();
 	void player_dmg_sound();
+	void player_die_sound();
 	void monster_atk_sound();
 	void monster_dmg_sound();
+	void monster_die_sound();
+	void charge_sound();
+	void skill1_sound();
+	void skill2_sound();
+	void skill3_sound();
 private:
 	Mix_Music* intro_bgm;
 	Mix_Music* stage_bgm;
 	Mix_Music* ending_bgm;
 	Mix_Music* gameover_bgm;
+	Mix_Chunk* phase_transition_se;
 	Mix_Chunk* btn_click_se;
 	Mix_Chunk* btn_hold_se;
 	Mix_Chunk* move_se;
 	Mix_Chunk* player_atk_se;
 	Mix_Chunk* player_dmg_se;
+	Mix_Chunk* player_die_se;
 	Mix_Chunk* monster_atk_se;
 	Mix_Chunk* monster_dmg_se;
-
+	Mix_Chunk* monster_die_se;
+	Mix_Chunk* charge_se;
+	Mix_Chunk* skill1_se;
+	Mix_Chunk* skill2_se;
+	Mix_Chunk* skill3_se;
 };
