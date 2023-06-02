68e4848f5339aa4b57f6c84a091715ded238f037
ultimatefirechamp <106022523+ultimatefirechamp@users.noreply.github.com>
2023-06-03 01:18:35 +0900

    aasdf

--- team_Project/main_Fun.cpp
+++ team_Project/main_Fun.cpp
@@ -20,7 +20,7 @@ int main(int arc, char** argv) {
 	GameManager* gm = GameManager::getinstance();
 	gm->g_renderer = SDL_CreateRenderer(win, -1, 0);
 
-	//gm->InitMap();
+	
 	InitGame();
 	//gm->map->SetTile();
 	//gm->PathFind(1, 8, 8, 1);
